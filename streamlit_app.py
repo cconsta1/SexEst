f35e3cce94f4b4e478a4bff5c74f47a3a06df538
Chrysovalantis Constantinou <cconsta1@Chrysovalantiss-MacBook-Pro.local>
2022-03-15 16:33:03 +0200

    Minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ st.set_page_config(
     page_title="SexEst",
     page_icon="skull",
     layout="wide"
-    # menu_items={
+    # menu_items =
+    # {
     #     'Get Help': 'https://www.extremelycoolapp.com/help',
     #     'Report a bug': "https://www.extremelycoolapp.com/bug",
     #     'About': "# This is a header. This is an *extremely* cool app!"
