e0fd4d0f0d3259b18ccfbe847af39616f967e4b6
cconsta1 <cconsta1@alumni.nd.edu>
2026-06-07 18:56:46 +0300

    Remove Beautiful Soup

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ import streamlit.components.v1 as components
 import toml
 import os
 import pathlib
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import logging
 import shutil
 
@@ -56,37 +56,37 @@ primaryColor = toml.load(".streamlit/config.toml")["theme"]["primaryColor"]
 # <style>
 # """
 
-def inject_ga():
-    GA_ID = "google_analytics"
+# def inject_ga():
+#     GA_ID = "google_analytics"
 
-    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
-    # web application's id is. You will find this in your Google Analytics account
+#     # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+#     # web application's id is. You will find this in your Google Analytics account
     
-    GA_JS = """
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+#     GA_JS = """
+#     <!-- Global site tag (gtag.js) - Google Analytics -->
+#     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+#     <script>
+#         window.dataLayer = window.dataLayer || [];
+#         function gtag(){dataLayer.push(arguments);}
+#         gtag('js', new Date());
 
-        gtag('config', 'G-XXXXXXXXXX');
-    </script>
-    """
+#         gtag('config', 'G-XXXXXXXXXX');
+#     </script>
+#     """
 
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):  # if cannot find tag
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  # recover from backup
-        else:
-            shutil.copy(index_path, bck_index)  # keep a backup
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+#     # Insert the script in the head tag of the static template inside your virtual
+#     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+#     logging.info(f'editing {index_path}')
+#     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+#     if not soup.find(id=GA_ID):  # if cannot find tag
+#         bck_index = index_path.with_suffix('.bck')
+#         if bck_index.exists():
+#             shutil.copy(bck_index, index_path)  # recover from backup
+#         else:
+#             shutil.copy(index_path, bck_index)  # keep a backup
+#         html = str(soup)
+#         new_html = html.replace('<head>', '<head>\n' + GA_JS)
+#         index_path.write_text(new_html)
 
 
 #inject_ga()
